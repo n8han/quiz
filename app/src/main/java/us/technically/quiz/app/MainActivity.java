f186da0aa4bbdc801792472bbb278f67c9ff63f3
Nathan Hamblen <nathan@technically.us>
2014-04-07 11:49:06 -0400

    separate host, joiner behavior

--- app/src/main/java/us/technically/quiz/app/MainActivity.java
+++ app/src/main/java/us/technically/quiz/app/MainActivity.java
@@ -20,6 +20,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 public class MainActivity extends Activity {
 
@@ -49,6 +53,7 @@ public class MainActivity extends Activity {
         final Button host = (Button) findViewById(R.id.host);
         host.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                registerReceiver(joinBroadcastReceiver, intentFilter);
                 mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
                     @Override
                     public void onSuccess() {
@@ -61,33 +66,58 @@ public class MainActivity extends Activity {
                 });
             }
         });
+        final Button join = (Button) findViewById(R.id.join);
+        join.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                registerReceiver(joinBroadcastReceiver, intentFilter);
+                mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Log.d(TAG, "discovering peers");
+                    }
+
+                    @Override
+                    public void onFailure(int reasonCode) {
+                        Log.d(TAG, "Failing to discover peers: " + reasonCode);
+                    }
+                });
+            }
+        });
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
     }
 
-    private WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+    private WifiP2pManager.PeerListListener hostPeerListListener = new WifiP2pManager.PeerListListener() {
+        private Set<String> availableDevices = new HashSet<String>();
+
         @Override
         public void onPeersAvailable(WifiP2pDeviceList peerList) {
             for (final WifiP2pDevice device : peerList.getDeviceList()) {
                 Log.d(TAG, "device available " + device.deviceName);
-                WifiP2pConfig config = new WifiP2pConfig();
-                config.deviceAddress = device.deviceAddress;
-                config.wps.setup = WpsInfo.PBC;
+                if (availableDevices.add(device.deviceAddress)) {
+                    Log.d(TAG, "device connecting " + device.deviceAddress);
+                    WifiP2pConfig config = new WifiP2pConfig();
+                    config.deviceAddress = device.deviceAddress;
+                    config.wps.setup = WpsInfo.PBC;
+                    config.groupOwnerIntent = 15;
 
-                mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
-                    @Override
-                    public void onSuccess() {
-                        // WiFiDirectBroadcastReceiver will notify us. Ignore for now.
-                    }
-                    @Override
-                    public void onFailure(int reason) {
-                        Toast.makeText(MainActivity.this, "Connect failed to " + device.deviceName, Toast.LENGTH_SHORT).show();
-                    }
-                });
+                    mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+                        @Override
+                        public void onSuccess() {
+                            // WiFiDirectBroadcastReceiver will notify us. Ignore for now.
+                        }
+
+                        @Override
+                        public void onFailure(int reason) {
+                            Toast.makeText(MainActivity.this, "Connect failed to " + device.deviceName, Toast.LENGTH_SHORT).show();
+                            availableDevices.remove(device.deviceAddress);
+                        }
+                    });
+                }
             }
         }
     };
-    class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+    private BroadcastReceiver hostBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -95,43 +125,53 @@ public class MainActivity extends Activity {
                 // Determine if Wifi P2P mode is enabled or not, whatever
             } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
                 Log.d(TAG, "P2P peers changed");
-                Toast.makeText(MainActivity.this, "peers changed", Toast.LENGTH_SHORT).show();
-                mManager.requestPeers(mChannel, peerListListener);
+                mManager.requestPeers(mChannel, hostPeerListListener);
             } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
                 Log.d(TAG, "Connection changed action");
                 NetworkInfo networkInfo = (NetworkInfo) intent
                         .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
-
+            }
+        }
+    };
+    private BroadcastReceiver joinBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+                // Determine if Wifi P2P mode is enabled or not, whatever
+            } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+                Log.d(TAG, "P2P peers changed");
+            } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+                Log.d(TAG, "Connection changed action");
+                NetworkInfo networkInfo = (NetworkInfo) intent
+                        .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
                 if (networkInfo.isConnected()) {
                     // we are connected with the other device, request connection
                     // info to find group owner IP
-
-                    mManager.requestConnectionInfo(mChannel, infoListener);
+                    mManager.requestConnectionInfo(mChannel, joinInfoListener);
                 }
-            } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
             }
         }
-    }
+    };
 
-    private WifiP2pManager.ConnectionInfoListener infoListener = new WifiP2pManager.ConnectionInfoListener() {
+    private WifiP2pManager.ConnectionInfoListener joinInfoListener = new WifiP2pManager.ConnectionInfoListener() {
         @Override
         public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
             Log.d(TAG, "groupOwnerAddress" + wifiP2pInfo.groupOwnerAddress);
         }
     };
 
-    private WiFiDirectBroadcastReceiver receiver;
     @Override
     public void onResume() {
         super.onResume();
-        receiver = new WiFiDirectBroadcastReceiver();
-        registerReceiver(receiver, intentFilter);
+/*        receiver = new WiFiDirectBroadcastReceiver();
+        registerReceiver(receiver, intentFilter);*/
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        unregisterReceiver(receiver);
+//        unregisterReceiver(receiver);
     }
 
     @Override
