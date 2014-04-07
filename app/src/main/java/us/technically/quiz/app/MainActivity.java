26db0fc542b130074ee4e7762d3f8a61d8131f43
Nathan Hamblen <nathan@technically.us>
2014-04-07 10:41:25 -0400

    can make connections and report an ip address

--- app/src/main/java/us/technically/quiz/app/MainActivity.java
+++ app/src/main/java/us/technically/quiz/app/MainActivity.java
@@ -5,7 +5,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.NetworkInfo;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -59,35 +64,61 @@ public class MainActivity extends Activity {
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
     }
-    private void setIsWifiP2pEnabled(boolean value) {
-        Log.d(TAG, "wifi p2p enabled: " + value);
-    }
+
+    private WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) {
+            for (final WifiP2pDevice device : peerList.getDeviceList()) {
+                Log.d(TAG, "device available " + device.deviceName);
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress = device.deviceAddress;
+                config.wps.setup = WpsInfo.PBC;
+
+                mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        // WiFiDirectBroadcastReceiver will notify us. Ignore for now.
+                    }
+                    @Override
+                    public void onFailure(int reason) {
+                        Toast.makeText(MainActivity.this, "Connect failed to " + device.deviceName, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+    };
     class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
-                // Determine if Wifi P2P mode is enabled or not, alert
-                // the Activity.
-                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                    setIsWifiP2pEnabled(true);
-                } else {
-                    setIsWifiP2pEnabled(false);
-                }
+                // Determine if Wifi P2P mode is enabled or not, whatever
             } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
                 Log.d(TAG, "P2P peers changed");
-                Toast.makeText(getApplicationContext(), "peers changed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "peers changed", Toast.LENGTH_SHORT).show();
+                mManager.requestPeers(mChannel, peerListListener);
             } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+                Log.d(TAG, "Connection changed action");
+                NetworkInfo networkInfo = (NetworkInfo) intent
+                        .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
 
-                // Connection state changed!  We should probably do something about
-                // that.
+                if (networkInfo.isConnected()) {
+                    // we are connected with the other device, request connection
+                    // info to find group owner IP
 
+                    mManager.requestConnectionInfo(mChannel, infoListener);
+                }
             } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
-                // device changed action what
             }
         }
     }
+
+    private WifiP2pManager.ConnectionInfoListener infoListener = new WifiP2pManager.ConnectionInfoListener() {
+        @Override
+        public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
+            Log.d(TAG, "groupOwnerAddress" + wifiP2pInfo.groupOwnerAddress);
+        }
+    };
 
     private WiFiDirectBroadcastReceiver receiver;
     @Override
